5eeb65829215826d52497edc775e651036cf94f4
Marian Sievers <marian.sievers@gmail.com>
2014-10-21 22:08:48 +0200

    Add install-part of zsh

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ echo "Start the setup of the dotfiles"
 # .osx install
 echo "Run .osx"
 ./.osx
+
+# zsh install
+echo "Install zsh-config"
+echo "Symlink the zsh-files to home"
+
+ln zsh/.zshrc ~/.zshrc
